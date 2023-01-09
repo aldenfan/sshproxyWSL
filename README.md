110d124c34d1819279348f4b5fa671b76fff5eea
Alden Fan <aldenhfan@gmail.com>
2023-01-09 15:23:37 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+sshproxyWSL
+
+Small scripts to wrap around NERSC's [sshproxy.sh](https://docs.nersc.gov/connect/mfa/#sshproxy). Install or link to a bin area, e.g. /usr/local/bin/ and then run sshproxyWSL.sh.
+
+Requirements:
+* 1password CLI
+* expect
---
+++ sshproxyWSL.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+# Generate ssh keys in both openssh and ppk formats and copy to windows side
+# 2023-01-09 Switched to pulling credentials from 1password. Requires using expect script.
+
+username=${USER}
+win_dir="/mnt/c/Users/alden"
+wsl_dir="/home/afan/.ssh"
+
+# get password and OTP from 1password
+pass="op://Personal/NERSC/password"
+otp=$(op item get nersc --otp)
+
+echo "Generate NERSC ssh key in OpenSSH format"
+#cmd="sshproxy.sh -u ${username}" # OLD
+echo "  User: ${username}"
+echo "  Password: pulling from 1password: ${pass}"
+echo "  OTP: pulling from 1password: ${otp}"
+
+cmd="/usr/local/bin/sshproxy.exp ${username} ${pass} ${otp}"
+#echo "cmd: ${cmd}"
+echo "===========SSHPROXY==========="
+$cmd
+echo ""
+echo "===========SSHPROXY==========="
+echo 
+echo "Copying NERSC ssh key to Windows"
+grep "ssh-rsa-cert" ${wsl_dir}/nersc > ${win_dir}/nersc.pub
+grep -v "ssh-rsa-cert" ${wsl_dir}/nersc > ${win_dir}/nersc
+
+
+#echo
+#echo "Generate NERSC ssh key in ppk format"
+#cmd2="sshproxy.sh -u ${username} -p -o /mnt/c/Users/alden/nersckey"
+#echo "cmd: ${cmd2}"
+#echo "===========SSHPROXY==========="
+#$cmd2
+#echo "===========SSHPROXY==========="
